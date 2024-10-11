be667d06fbac2310cc4a5cde73eccf78553b005f
Simran <simran0ray@example.com>
2024-10-11 16:44:51 +0530

    routing updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Cart from './pages/Cart';
+import Collection from './pages/Collection';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import Orders from './pages/Orders';
+import PlaceOrder from './pages/PlaceOrder';
+import Product from './pages/Product';
 
 const App = () => {
   return (
-    <h1 className='text-3xl text-pink-300'>
-      hello
-    </h1>
+    <div>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/orders' element={<Orders />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/product:productId' element={<Product />} />
+      </Routes>
+    </div>
   )
 }
 
