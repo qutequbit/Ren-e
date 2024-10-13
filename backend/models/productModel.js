caddc1481e087288a63aed8a46fd7f99f49b8e73
Simran <simran0ray@example.com>
2024-10-13 09:13:32 +0530

    backend

---
+++ backend/models/productModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+//schema
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: Array, required: true },
+    category: { type: String, required: true },
+    subCategory: { type: String, required: true },
+    sizes: { type: Array, required: true },
+    bestSeller: { type: Boolean },
+    data: { type: Number, required: true },
+})
+
+//creating the model
+
+const productModel = mongoose.models.product || mongoose.model("product", productSchema)
+
+export default productModel;
