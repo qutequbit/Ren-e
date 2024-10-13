7d6a927fce0121b4b1af69aac83ca44fc1a3006f
Simran <simran0ray@example.com>
2024-10-13 19:12:13 +0530

    backend

---
+++ backend/controllers/productController.js
@@ -0,0 +1,93 @@
+//1. add products
+//2. list products
+//3. remove product
+//4. single product info
+
+import { v2 as cloudinary } from 'cloudinary'
+import productModel from "../models/productModel.js"
+
+//1
+const addProduct = async (req, res) => {
+    //creating a middleware using multer
+    try {
+        const { name, description, price, category, subCategory, sizes, bestseller } = res.body
+
+        const image1 = req.files.image1 && req.files.image1[0]
+        const image2 = req.files.image2 && req.files.image2[0]
+        const image3 = req.files.image3 && req.files.image3[0]
+        const image4 = req.files.image4 && req.files.image4[0]
+
+        const images = [image1, image2, image3, image4].filter((item) => item !== undefined)
+
+        let imagesUrl = await Promise.all(
+            images.map(async (item) => {
+                let result = await cloudinary.uploader.upload(item.path, { resource_type: 'image' })
+                return result.secure_url
+            })
+        )
+
+        const productData = {
+            name,
+            description,
+            price: Number(price),
+            subCategory,
+            bestseller: bestseller === "true" ? true : false,
+            sizes: JSON.parse(sizes),
+            image: imagesUrl,
+            date: Date.now()
+        }
+
+        const product = new productModel(productData)
+        await product.save()
+
+        res.json({ success: true, message: "Product Added" })
+        // console.log({ name, description, price, category, subCategory, sizes, bestseller });
+        // console.log(image1, image2, image3, image4);
+        //upload the images in cloudinary and then store in the database
+        res.json({})
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
+}
+
+//2
+const listProducts = async (req, res) => {
+    try {
+        const products = await productModel.find({})
+        res.json({ success: true, products })
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
+}
+
+//3
+const removeProduct = async (req, res) => {
+    try {
+
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({ success: true, message: "Product Removed" })
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
+}
+
+//4
+const singleProduct = async (req, res) => {
+    try {
+
+        const { productId } = req.body
+        const product = await productModel.findById(productId)
+        res.json({ success: true, productId })
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
+}
+
+export { listProducts, addProduct, removeProduct, singleProduct }
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
 
 
 //App Configuration
@@ -17,6 +19,9 @@ app.use(express.json())
 app.use(cors())
 
 //API endpoints
+app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
+
 app.get('/', (reg, res) => {
     res.send('API Working')
 })
